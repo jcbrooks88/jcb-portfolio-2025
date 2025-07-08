871bc1272248dbbff20e21e62c41c2a178d30c31
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-07-08 16:43:44 -0400

    WRNC title update

--- portfolio/src/projects/WRNC.ts
+++ portfolio/src/projects/WRNC.ts
@@ -1,6 +1,6 @@
 export const wellness = [
     {
-      title: "Wellness Rounds NC",
+      title: "Wellness Rounds NC - Healthcare Professional Discussion Board",
       techStack: [
         "React + Vite + Apollo Client",
         "Node.js",
@@ -10,11 +10,10 @@ export const wellness = [
         "Typescript",
       ],
       description:
-        "A platform for safe space discussions among healthcare professionals.",
+        "A platform for safe space to create or contribute to discussions among healthcare professionals.",
       github: "https://github.com/jcbrooks88/WELLNESSROUNDS-NC",
       demo: "https://youtu.be/EYYJw8g3Gjo",
       image: "/assets/WRNC.png",
     },
-    // Add more projects here as needed
   ];
   
